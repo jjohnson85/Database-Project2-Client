990e4223f2b29d4f9d4637df6e0fe3b0afa80e52
FrustyLove <jared.t.johnson@mines.sdsmt.edu>
2016-04-12 13:20:33 -0600

    Fault form more or less done

--- Project2Client/Project2Client/FaultMangmentForm.Designer.cs
+++ Project2Client/Project2Client/FaultMangmentForm.Designer.cs
@@ -28,12 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.s7110008DataSet = new Project2Client.s7110008DataSet();
+            this.electFaultTableAdapter = new Project2Client.s7110008DataSetTableAdapters.ElectFaultTableAdapter();
+            this.mechFaultTableAdapter = new Project2Client.s7110008DataSetTableAdapters.MechFaultTableAdapter();
+            this.otherFaultTableAdapter = new Project2Client.s7110008DataSetTableAdapters.OtherFaultTableAdapter();
+            this.bindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.bindingSource3 = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.s7110008DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource3)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -43,6 +55,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(447, 227);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // button1
             // 
@@ -86,6 +99,38 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // bindingSource1
+            // 
+            this.bindingSource1.DataMember = "OtherFault";
+            this.bindingSource1.DataSource = this.s7110008DataSet;
+            // 
+            // s7110008DataSet
+            // 
+            this.s7110008DataSet.DataSetName = "s7110008DataSet";
+            this.s7110008DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // electFaultTableAdapter
+            // 
+            this.electFaultTableAdapter.ClearBeforeFill = true;
+            // 
+            // mechFaultTableAdapter
+            // 
+            this.mechFaultTableAdapter.ClearBeforeFill = true;
+            // 
+            // otherFaultTableAdapter
+            // 
+            this.otherFaultTableAdapter.ClearBeforeFill = true;
+            // 
+            // bindingSource2
+            // 
+            this.bindingSource2.DataMember = "MechFault";
+            this.bindingSource2.DataSource = this.s7110008DataSet;
+            // 
+            // bindingSource3
+            // 
+            this.bindingSource3.DataMember = "ElectFault";
+            this.bindingSource3.DataSource = this.s7110008DataSet;
+            // 
             // FaultMangmentForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -100,16 +145,26 @@
             this.Text = "FaultMangmentForm";
             this.Load += new System.EventHandler(this.FaultMangmentForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.s7110008DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource3)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private s7110008DataSet s7110008DataSet;
+        private s7110008DataSetTableAdapters.ElectFaultTableAdapter electFaultTableAdapter;
+        private s7110008DataSetTableAdapters.MechFaultTableAdapter mechFaultTableAdapter;
+        private s7110008DataSetTableAdapters.OtherFaultTableAdapter otherFaultTableAdapter;
+        private System.Windows.Forms.BindingSource bindingSource2;
+        private System.Windows.Forms.BindingSource bindingSource3;
     }
 }
--- Project2Client/Project2Client/FaultMangmentForm.cs
+++ Project2Client/Project2Client/FaultMangmentForm.cs
@@ -12,16 +12,38 @@ namespace Project2Client
 {
     public partial class FaultMangmentForm : Form
     {
+
+        private string select;
         public FaultMangmentForm()
         {
             InitializeComponent();
         }
-
-
+        
+        
         //Save
         private void button1_Click(object sender, EventArgs e)
         {
-
+            select = comboBox1.Text;
+            switch (select)
+            {
+                case "Mechanical Fault":
+                    this.Validate();
+                    this.bindingSource2.EndEdit();
+                    this.mechFaultTableAdapter.Update(this.s7110008DataSet.MechFault);
+                    break;
+                case "Electrical Fault":
+                    this.Validate();
+                    this.bindingSource3.EndEdit();
+                    this.electFaultTableAdapter.Update(this.s7110008DataSet.ElectFault);
+                    break;
+                case "Other Fault":
+                    this.Validate();
+                    this.bindingSource1.EndEdit();
+                    this.otherFaultTableAdapter.Update(this.s7110008DataSet.OtherFault);
+                    break;
+                default:
+                    break;
+            }
         }
 
         //Delete
@@ -32,10 +54,37 @@ namespace Project2Client
 
         private void FaultMangmentForm_Load(object sender, EventArgs e)
         {
-
+            // TODO: This line of code loads data into the 's7110008DataSet.OtherFault' table. You can move, or remove it, as needed.
+            this.otherFaultTableAdapter.Fill(this.s7110008DataSet.OtherFault);
+            // TODO: This line of code loads data into the 's7110008DataSet.MechFault' table. You can move, or remove it, as needed.
+            this.mechFaultTableAdapter.Fill(this.s7110008DataSet.MechFault);
+            // TODO: This line of code loads data into the 's7110008DataSet.ElectFault' table. You can move, or remove it, as needed.
+            this.electFaultTableAdapter.Fill(this.s7110008DataSet.ElectFault);
+            this.dataGridView1.DataSource = this.bindingSource1;
         }
 
+        //Change fault-type table displayed
         private void button3_Click(object sender, EventArgs e)
+        {
+            select = comboBox1.Text;
+            switch (select)
+            {
+                case "Mechanical Fault":
+                    this.dataGridView1.DataSource = this.bindingSource2;
+                    break;
+                case "Electrical Fault":
+                    this.dataGridView1.DataSource = this.bindingSource3;
+                    break;
+                case "Other Fault":
+                    this.dataGridView1.DataSource = this.bindingSource1;
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
